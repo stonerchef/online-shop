b523ce6f4c2473030bccbce359ba060d0337cef6
S4bril <filip.informatyka@vp.pl>
2023-01-23 01:03:23 +0100

    start server

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+const PORT = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', '/views');
+
+app.get('/', (req, res) => {
+    res.send("halooooo"); //jak chesz strone odesłać to tak: res.render('strona'); 
+                            //gdzie strona to plik strona.ejs i znajduje sie w folderze views
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
+})
+  
+app.listen(PORT, () => {
+    console.log(`server started at adress localhost/${PORT}...`)
+});
