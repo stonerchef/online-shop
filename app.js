05c972faea4a5c1fa1581c9135bbe6f6bc6c2e78
S4bril <filip.informatyka@vp.pl>
2023-01-23 01:12:03 +0100

    add rendering test.ejs to app.js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', '/views');
 
 app.get('/', (req, res) => {
-    res.send("halooooo"); //jak chesz strone odesłać to tak: res.render('strona'); 
-                            //gdzie strona to plik strona.ejs i znajduje sie w folderze views
+    res.render('test');
 })
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1>resource not found</h1>')
+    res.status(404).send('<h1>resource not found</h1>');
 })
   
 app.listen(PORT, () => {
-    console.log(`server started at adress localhos:${PORT}...`)
+    console.log(`server started at adress localhos:${PORT}...`);
 });
