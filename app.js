10c3fd64bbb7732b6a68f3dec8af53b0236288bc
S4bril <95976169+S4bril@users.noreply.github.com>
2023-01-23 02:06:04 +0100

    change name adad.js to app.js and make simple server application

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+//app.set('views', '/views'); jakaś literówka tu jest pewnie w ścieżce
+
+app.get('/', (req, res) => {
+    res.render('test');
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>');
+})
+  
+app.listen(PORT, () => {
+    console.log(`server started at adress localhos:${PORT}...`);
+});
